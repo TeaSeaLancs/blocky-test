14c63e477e0f856a499f4b2112ed090a6a653d47
Matt Harker <matt@theflux.co.uk>
2019-07-12 11:02:36 +0100

    Cleanup

--- src/BlockyGame.test.js
+++ src/BlockyGame.test.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { renderHook, act } from '@testing-library/react-hooks';
 
 import BlockyGame from './BlockyGame';
 
 import createSampleGrid from './__fixtures__/SampleGrid';
-
-// We mock out the removeBlockAndUpdateGrid import here because we don't want to have to test its logic
-// while we're testing this component.
-jest.mock('./util/removeBlockAndUpdateGrid');
 
 describe('BlockyGame', () => {
   let grid;
